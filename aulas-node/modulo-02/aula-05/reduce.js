2bdc220faa4098859089be830b65ff08a2396e31
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-10-08 18:49:22 -0300

    Demo relacionado ao uso de Array.Reduce

---
+++ aulas-node/modulo-02/aula-05/reduce.js
@@ -0,0 +1,32 @@
+/**
+ * File: reduce.js
+ * Description: Demo sobre uso do Array.Reduce
+ * Date: 17/01/2019
+ * Author: Glaucia Lemos
+ */
+
+const {
+  obterPessoas
+} = require('./service')
+
+async function main() {
+  try {
+    const {
+      results
+    } = await obterPessoas(`a`);
+
+    const pesos = results.map(item => parseInt(item.height));
+    console.log('Pesos...: ', pesos);
+
+    const total = pesos.reduce((anterior, proximo) => {
+      return anterior + proximo;
+    })
+
+    console.log('Total....: ', total);
+
+  } catch (error) {
+    console.log(`Erro na execucao do código`, error);
+  }
+}
+
+main();
