5aa44ce9f74226d2816ff2e218b3310120490a14
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-10-18 13:26:03 -0300

    commit

--- aulas-node/modulo-01/aula-02/index.js
+++ aulas-node/modulo-01/aula-02/index.js
@@ -12,55 +12,61 @@
  */
 
 function obterUsuario(callback) {
-    setTimeout(() => callback(null, {
-        id: 1,
-        nome: "Glaucia Lemos",
-        dataNascimento: new Date(),
-    }), 1000);
+  setTimeout(() => {
+    return callback(null, {
+      id: 1,
+      nome: 'Glaucia Lemos',
+      dataNascimento: new Date(),
+    });
+  }, 1000);
 }
 
 function obterTelefone(idUsuario, callback) {
-    setTimeout(() => callback(null, {
-        telefone: "99999-9999",
-        ddd: 21,
-    }), 2000);
+  setTimeout(() => {
+    return callback(null, {
+      telefone: '99999-9999',
+      ddd: 21,
+    });
+  }, 2000);
 }
 
 function obterEndereco(idUsuario, callback) {
-    setTimeout(() => callback(null, {
-        rua: "Jorge Yunes",
-        numero: 181,
-        complemento: 103,
-        bairro: "Recreio dos Bandeirantes",
-        cidade: "Rio de Janeiro",
-    }), 2000);
+  setTimeout(() => {
+    return callback(null, {
+      rua: 'Jorge Yunes',
+      numero: 181,
+      complemento: 103,
+      bairro: 'Recreio dos Bandeirantes',
+      cidade: 'Rio de Janeiro',
+    });
+  }, 2000);
 }
 
 function resolverUsuario(error, usuario) {
-    console.log("Usuario", usuario);
+  console.log('Usuario', usuario);
 }
 
 obterUsuario((error, usuario) => {
-    // valor: null || "" || 0 === false
-    if (error) {
-        console.log(`Erro ao encontrar o Usuário...: ${error}`);
+  // valor: null || "" || 0 === false
+  if (error) {
+    console.log(`Erro ao encontrar o Usuário...: ${error}`);
+  }
+  obterTelefone(usuario.id, (error1, telefone) => {
+    if (error1) {
+      console.log(`Erro ao encontrar o Telefone...: ${error1}`);
     }
-    obterTelefone(usuario.id, (error1, telefone) => {
-        if (error1) {
-            console.log(`Erro ao encontrar o Telefone...: ${error1}`);
-        }
 
-        obterEndereco(usuario.id, (error2, endereco) => {
-            if (error2) {
-                console.log(`Erro ao encontrar o Endereço...:${error2}`);
-            }
+    obterEndereco(usuario.id, (error2, endereco) => {
+      if (error2) {
+        console.log(`Erro ao encontrar o Endereço...:${error2}`);
+      }
 
-            // Impressão dos Dados:
-            console.log(`
+      // Impressão dos Dados:
+      console.log(`
               Nome: ${usuario.nome}
               Endereço: ${endereco.rua}, ${endereco.numero}/${endereco.complemento} - ${endereco.bairro}, ${endereco.cidade}
               Telefone: (${telefone.ddd})${telefone.telefone}
             `);
-        });
     });
+  });
 });
