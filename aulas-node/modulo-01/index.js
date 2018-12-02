a353c22d180d4235790dec96683431e3c6aabb07
Glaucia Lemos <glaucia86>
2018-12-02 21:23:52 -0200

    Exercício sobre uso de callbacks em funcoes.

--- aulas-node/modulo-01/index.js
+++ aulas-node/modulo-01/index.js
@@ -11,27 +11,60 @@
  * 2 - Obter o endereço do usuário pelo id
  */
 
-function obterUsuario() {
-    setTimeout(() => ({
+function obterUsuario(callback) {
+    setTimeout(() => callback(null, {
         id: 1,
-        nome: "Glaucia",
+        nome: "Glaucia Lemos",
         dataNascimento: new Date(),
     }), 1000);
 }
 
-function obterTelefone(idUsuario) {
-    setTimeout(() => ({
-        telefone: "21912787",
+function obterTelefone(idUsuario, callback) {
+    setTimeout(() => callback(null, {
+        telefone: "99999-9999",
         ddd: 21,
     }), 2000);
 }
 
-function obterEndereco(idUsuario) {
-
+function obterEndereco(idUsuario, callback) {
+    setTimeout(() => callback(null, {
+        rua: "Jorge Yunes",
+        numero: 181,
+        complemento: 103,
+        bairro: "Recreio dos Bandeirantes",
+        cidade: "Rio de Janeiro",
+    }), 2000);
 }
 
-const usuario = obterUsuario();
-const telefone = obterTelefone(usuario.id);
+function resolverUsuario(error, usuario) {
+    console.log("Usuario", usuario);
+}
 
-console.log("Usuario", usuario);
-console.log("Telefone", usuario);
+obterUsuario((error, usuario) => {
+    // valor: null || "" || 0 === false
+    if (error) {
+        console.log(`Erro ao encontrar o Usuário...: ${error}`);
+    }
+    obterTelefone(usuario.id, (error1, telefone) => {
+        if (error1) {
+            console.log(`Erro ao encontrar o Telefone...: ${error1}`);
+        }
+
+        obterEndereco(usuario.id, (error2, endereco) => {
+            if (error2) {
+                console.log(`Erro ao encontrar o Endereço...:${error2}`);
+            }
+
+            // Impressão dos Dados:
+            console.log(`
+              Nome: ${usuario.nome}
+              Endereço: ${endereco.rua}, ${endereco.numero}/${endereco.complemento} - ${endereco.bairro}, ${endereco.cidade}
+              Telefone: (${telefone.ddd})${telefone.telefone}
+            `);
+        });
+    });
+});
+
+
+// const telefone = obterTelefone(usuario.id);
+// console.log("Telefone", usuario);
