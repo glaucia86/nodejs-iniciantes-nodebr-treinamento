d00fd015524f48367a33a6c175504303843ee4d7
Glaucia Lemos <glaucia86>
2018-12-02 23:20:54 -0200

    Exercícios sobre uso de Events em node.js

---
+++ aulas-node/modulo-01/aula-07/index.js
@@ -0,0 +1,35 @@
+/**
+ * File: index.js
+ * Description: Demonstração de como fazer uso de de Events
+ * Date: 12/02/2018
+ * Author: Glaucia Lemos
+ */
+
+const EventEmitter = require('events');
+
+class Emissor extends EventEmitter {
+
+}
+
+const emissor = new Emissor();
+const evento = 'usuario:click';
+
+emissor.on(evento, (click) => {
+  console.log('1 - Clicou', click);
+});
+
+
+/* emissor.emit(evento, 'Barra de Rolagem');
+emissor.emit(evento, 'Clicou Botão ok');
+
+let count = 0;
+
+setInterval(() => {
+  // eslint-disable-next-line no-plusplus
+  emissor.emit(evento, `Clicou Botão Ok...: ${count++}`);
+}, 1000); */
+
+const stdin = process.openStdin();
+stdin.addListener('data', (value) => {
+  console.log(`Você digitou...: ${value.toString().trim()}`);
+});
