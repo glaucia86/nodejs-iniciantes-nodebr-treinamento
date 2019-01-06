e25d845f017587738d3c78dff3980cab7976924b
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2019-01-06 19:01:49 -0200

    Demo relacionado ao uso de Array.Map

--- aulas-node/modulo-01/aula-08/index.js
+++
@@ -1,6 +0,0 @@
-/**
- * File: index.js
- * Description: Demonstração de como fazer uso de de Array.Map
- * Date: 17/01/2019
- * Author: Glaucia Lemos
- */
---
+++ aulas-node/modulo-02/aula-03/map.js
@@ -0,0 +1,39 @@
+/**
+ * File: map.js
+ * Description: Demo sobre uso do Array.Map
+ * Date: 17/01/2019
+ * Author: Glaucia Lemos
+ */
+
+const service = require('./service');
+
+// Aqui uma demonstraçao de como o Map funciona por debaixo dos panos:
+Array.prototype.meuMap = function (callback) {
+  const novoArrayMapeado = [];
+  for (let index = 0; index <= this.length - 1; index++) {
+    const resultado = callback(this[index], index);
+    novoArrayMapeado.push(resultado);
+  }
+
+  return novoArrayMapeado;
+}
+
+async function main() {
+  try {
+    const results = await service.obterPessoas(`a`);
+    /* const names = results.results.map((pessoa) => {
+      return pessoa.name;
+    }) */
+    // Aqui uma maneira mais elegante de usar o 'map':
+    //const names = results.results.map((pessoa) => pessoa.name)
+    const names = results.results.meuMap((pessoa, index) => {
+      return `[${index}]${pessoa.name}`;
+    });
+
+    console.log('Names....: ', names);
+  } catch (error) {
+    console.log(`Erro na execução do código...: `, error);
+  }
+}
+
+main();
