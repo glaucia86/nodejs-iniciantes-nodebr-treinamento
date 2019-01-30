ad3dd4e458f7300fd2105e83e8bf0dcc537ad69b
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2019-01-30 12:28:28 -0200

    Alteracao nas regras do eslint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,38 @@
-export default {
+module.exports = {
   "extends": "airbnb-base",
-  "plugins": [
-    "import"
-  ],
+  "globals": {
+    $: true,
+    "window": true,
+    "document": true
+  },
+  "plugins": ["import"],
   "rules": {
     "no-console": "off",
     "import/newline-after-import": "off",
+    "global-require": "off",
+    "guard-for-in": "off",
+    "no-undef": "off",
+    "import/no-unresolved": "off",
+    "no-param-reassign": "off",
+    "no-shadow": "off",
+    "radix": "off",
+    "no-return-await": "off",
+    "import/extensions": "off",
+    "no-multi-assign": "off",
+    "import/no-dynamic-require": "off",
+    "prefer-destructuring": "off",
+    "no-alert": "off",
+    "func-names": "off",
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        "devDependencies": true
+      }
+    ],
+    "eslint-disable-next-line": 0,
+    "no-restricted-syntax": 0,
+    "no-prototype-builtins": 0,
     "no-unused-vars": 0,
-    "max-len": 0,
-    "arrow-body-style": ["error", "always"]
+    "max-len": 0
   }
 };
